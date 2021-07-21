e547a307ee14d0882d62a565e196fbbdf3ecc14a
Gabriel Marques <gabriel_frahm@hotmail.com>
2021-07-21 16:48:32 -0300

    initial

---
+++ start/routes.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.on('/').render('welcome')
+
+
+// room
+Route
+  .group(() => {
+    Route.post('', 'RoomController.create')
+
+    Route.get(':id', 'RoomController.select')
+    Route.post(':id', 'RoomController.createMessage')
+  })
+  .prefix('/rooms')
